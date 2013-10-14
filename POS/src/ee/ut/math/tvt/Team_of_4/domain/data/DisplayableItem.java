5f63993e53ddf1a19d7a310ca9047aefa6f488b2
Joosep Järve <joosep.jarve@gmail.com>
2013-10-14 15:44:17 +0300

    ant test 123

---
+++ POS/src/ee/ut/math/tvt/Team_of_4/domain/data/DisplayableItem.java
@@ -0,0 +1,11 @@
+package ee.ut.math.tvt.Team_of_4.domain.data;
+
+/**
+ * Base interface for data items, so one JTable can be used to display different entities.
+ */
+public interface DisplayableItem {
+	/**
+	 * Id of entity.
+	 */
+	public Long getId();
+}
