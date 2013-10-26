d0ccb606650bc78228e75b36142c3a3282e376c9
Karlasa <karlasaaremae@gmail.com>
2013-10-26 21:58:46 +0300

    Historytab UI update

--- POS/src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
+++ POS/src/ee/ut/math/tvt/salessystem/ui/tabs/HistoryTab.java
@@ -1,21 +1,51 @@
 package ee.ut.math.tvt.salessystem.ui.tabs;
 
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+import java.util.Date;
+
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
  * labelled "History" in the menu).
  */
 public class HistoryTab {
-    
+	
+	private Object rowData[][] = {};
+	private Object columnNames[] = { "Date", "Time", "Order price" };
+	
     // TODO - implement!
 
     public HistoryTab() {} 
     
     public Component draw() {
+    	
         JPanel panel = new JPanel();
+    	JTable table = new JTable(rowData, columnNames);        
+    	
+    	
+        JTableHeader header = table.getTableHeader();
+        header.setReorderingAllowed(false);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        GridBagConstraints gc = new GridBagConstraints();
+        GridBagLayout gb = new GridBagLayout();
+        gc.fill = GridBagConstraints.BOTH;
+        gc.weightx = 1.0;
+        gc.weighty = 1.0;
+
+        panel.setLayout(gb);
+        panel.add(scrollPane, gc);
+        
+        
         // TODO - Sales history tabel
         return panel;
     }
