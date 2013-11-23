e4d975e5344b5463cdae1b72c938c69a37b9c85b
SixtenLeemets <suix@hot.ee>
2013-11-23 15:35:20 +0200

    stuck here atm

---
+++ POS/test/ee/ut/math/tvt/salessystem/test/SingleSaleTest.java
@@ -0,0 +1,36 @@
+package ee.ut.math.tvt.salessystem.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class SingleSaleTest {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	//TODO
+	testAddSoldItem(){
+
+	}
+	
+	//TODO
+	testGetSumWithNoItems(){
+		
+	}
+	
+	//TODO
+	testGetSumWithOneItem(){
+		
+	}
+	
+	//TODO
+	testGetSumWithMultipleItems(){
+		
+	}	
+}
